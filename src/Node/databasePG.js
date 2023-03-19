bde7cefd1df971aa7e61074ee956db86bf733706
andrewql01 <114111690+andrewql01@users.noreply.github.com>
2023-03-19 11:01:44 +0100

    message

--- src/Node/databasePG.js
+++ src/Node/databasePG.js
@@ -7,14 +7,5 @@ const client = new Client({
   port: 5432,
 });
 client.connect();
-const GetUsers = (req,res) => {
-  client.query("Select * from chatusers",(err,results)=>{
-  if(err){
-    //error
-  }
-  else{
-    res.status(200).send(results.rows)
-  }
-});
-}
-module.exports = {GetUsers}
+
+module.exports = {client}
--- src/Node/firebaseAdmin.js
+++ src/Node/firebaseAdmin.js
@@ -1,11 +1,26 @@
-const {initializeApp} = require('firebase-admin/app')
+var admin = require("firebase-admin");
+var {client} = require("./databasePG");
+var serviceAccount = require("./service/serviceAccountKey.json");
 
-const app = initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chat-web-app-ab848-default-rtdb.europe-west1.firebasedatabase.app"
+})
 
-app.auth().listUsers()
+admin.auth().listUsers()
     .then((listUsersResult) => {
         listUsersResult.users.forEach((userRecord) => {
-            console.log('user',userRecord.toJSON());
+            const user = userRecord.toJSON();
+            client.query('INSERT INTO chatUsers (id, name, email) VALUES ($1, $2, $3)', [user.uid, user.email, user.displayName], (err,res) => {
+                if(err){
+                    //error
+                    console.log('error')
+                  }
+                else{
+                    //success
+                    console.log('success inserting users')
+                }
+            })
         });
     })
     .catch((error) => {
